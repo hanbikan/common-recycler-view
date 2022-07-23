daa9d5a88b9f26f1d05b8b143ee7493a3532aab2
Hanbit-Kang <fchopinof99@gmail.com>
2022-07-23 15:33:36 +0900

    Remove unused libraries

--- app/src/main/java/com/example/commonrecyclerview/viewholder/CommonViewHolder.kt
+++ app/src/main/java/com/example/commonrecyclerview/viewholder/CommonViewHolder.kt
@@ -6,7 +6,7 @@ import com.example.commonrecyclerview.data.CommonItem
 
 /** 새로운 뷰타입이 생길 때마다 자식 뷰홀더를 추가해야 합니다. */
 abstract class CommonViewHolder(
-        binding: ViewDataBinding
-    ) : RecyclerView.ViewHolder(binding.root) {
+    binding: ViewDataBinding
+) : RecyclerView.ViewHolder(binding.root) {
     abstract fun bind(item: CommonItem)
 }
